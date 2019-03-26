1f547ce9fb7a80dd7ddd583a18dd4c9dc2b95865
luckyadam <weitaozsh@gmail.com>
2019-03-26 21:35:22 +0800

    更新依赖

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     "presets":["@babel/preset-env"],
-    "plugins":["@babel/plugin-transform-modules-commonjs"],
+    "plugins":[
+      "@babel/plugin-transform-modules-commonjs",
+      "@babel/plugin-proposal-class-properties"
+    ],
     "babelrcRoots": [".", "node_modules"]
   };
 }
